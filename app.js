7733ade3e1da78e071f59aceed2e731e11b1be1d
encorey <lukasz.jagodka@protonmail.com>
2020-12-29 02:19:12 +0100

    Added post /signup router

--- app.js
+++ app.js
@@ -8,7 +8,43 @@ app.listen(3001, () =>{
 })
 
 app.post('/signup', (req, res) => {
-    res.send('Signup')
+    let name = req.body.name;
+    let email = req.body.email;
+    let password = req.body.password
+
+    User.findOne({where:{ email: email }})
+    .then(emailUser =>{
+        if(emailUser == null){
+            let token = null;
+            crypto.randomBytes(45, (err, buf) => {
+                if (err) throw err;
+                return token = buf.toString('hex')
+              });
+            bcrypt.hash(password, 10, async (err, hash) => {
+                try {
+                    await User.create({
+                        name,
+                        email,
+                        password: hash,
+                        active: true,
+                        token: token
+                    },{
+                        fields: ['name','email','password', 'active', 'token']
+                    }).then(user => {
+                        
+                        return res.json({
+                            success: true,
+                            code: 200
+                        })
+                    }).catch(err => console.log(err))
+                }catch (err) {
+                    console.error(err);
+                }
+            })
+        }else{
+            return res.sendStatus(400);
+        }
+    })
 })
 
 app.post('/login', (req, res) => {
