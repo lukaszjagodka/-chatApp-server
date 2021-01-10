ca12becf0604a071263b4d584112f3c0b4b6fb7e
encorey <lukasz.jagodka@protonmail.com>
2021-01-10 17:48:16 +0100

    added WebSocket

--- app.js
+++ app.js
@@ -16,7 +16,22 @@ const Op = Sequelize.Op;
 
 app.use(express.json());
 
-app.listen(3001, () =>{
+const WebSocket = require('ws');
+const server = require('http').createServer(app);
+
+const wss = new WebSocket.Server({ server: server });
+
+wss.on('connection', function connection(ws) {
+  console.log('new client conn');
+  ws.send('welcome new client');
+
+  ws.on('message', function incoming(data) {
+    console.log('reciverd ', data)
+  
+  });
+});
+
+server.listen(3001, () =>{
     console.log("Server working");
 })
 
